0bd0499d9b559139b6826a2e4bdcf875cb35179b
coolriff <coolriff@gmail.com>
2015-01-26 16:30:19 +0000

    Lab 2 final demo version
    cube color differet

--- Animation/Cube.cpp
+++ Animation/Cube.cpp
@@ -5,6 +5,7 @@ Cube::Cube(glm::vec3 &p, glm::quat &o, glm::vec3 &s, float d) : RigidBody(p, o, 
 	float mass = d * m_scale.x * m_scale.y * m_scale.z;
 	m_massInverse = 1.0f / mass;
 
+
 	glm::mat3 I = mass/12.0f * glm::mat3(
 		glm::vec3(m_scale.y * m_scale.y + m_scale.z * m_scale.z, 0, 0),
 		glm::vec3(0, m_scale.x * m_scale.x + m_scale.z * m_scale.z, 0),
